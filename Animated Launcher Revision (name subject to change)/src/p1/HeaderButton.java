294966c6336d6ec1c39275b0d02f65efed26d12b
aw3someone <aw3someone@gmail.com>
2016-04-04 18:17:32 -0700

    to do list

--- Animated Launcher Revision (name subject to change)/src/p1/HeaderButton.java
+++ Animated Launcher Revision (name subject to change)/src/p1/HeaderButton.java
@@ -2,6 +2,7 @@ package p1;
 
 public class HeaderButton extends ImageButton {
 
+//     lol useless constructor - need to refactor
     public HeaderButton(int category) {
         super(category);
     }
